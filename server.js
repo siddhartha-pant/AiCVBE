799cddf2d09be335b596fab0dab7cf2db0c2b8f3
siddhartha-pant <iamsiddhartha23@gmail.com>
2026-04-19 13:44:10 +0530

    chnaged server file

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const connectToDB = require("./src/config/database");
 
 connectToDB();
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
