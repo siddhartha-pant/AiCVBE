65e6bf5b406ffc0b59238d316af3e4fe15632b24
siddhartha-pant <iamsiddhartha23@gmail.com>
2026-04-19 13:47:40 +0530

    port coming from env

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ connectToDB();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
+const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
